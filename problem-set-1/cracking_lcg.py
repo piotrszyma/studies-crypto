74b0efe02151392cc87abc0c345b6a982ba6f7f2
Piotr Szyma <thompson2908@gmail.com>
2019-03-16 22:37:50 +0100

    glibc random

--- problem-set-1/cracking_lcg.py
+++ problem-set-1/cracking_lcg.py
@@ -5,6 +5,8 @@ import subprocess
 import utils
 
 
+CONST_2_32 = 2 ** 32
+
 class LCG:
   multiplier = 1103515245
   increment = 12345
@@ -47,13 +49,101 @@ def solve_with_all_unknown(samples):
       modulus, samples)
   return modulus, multiplier, increment
 
+def is_valid(outputs, shifts, index):
+  r_i = (outputs[index] << 1) + shifts[index]
+  r_i_28 = (outputs[index + 28] << 1) + shifts[index + 28]
+  r_i_31 = (r_i + r_i_28) % CONST_2_32
+  o_i_31 = r_i_31 >> 1
+  is_same = o_i_31 == outputs[index + 31]
+  return is_same
+
+
+def calc_next(outputs, shifts, first_unknown):
+  if first_unknown == 28:
+    return shifts[:31]
+    # import pdb; pdb.set_trace()
+    # return shifts.copy() # what to return?
+
+  for first, sec in ((0, 0), (0, 1), (1, 1), (1, 0)):
+    shifts = shifts.copy()
+    shifts[first_unknown] = first
+    shifts[first_unknown + 28] = sec
+    if is_valid(outputs, shifts, first_unknown):
+      value = calc_next(outputs, shifts, first_unknown + 1)
+      return value
+      # import pdb; pdb.set_trace()
+  else:
+    import pdb; pdb.set_trace()
+  return False
+    # else:
+    #   shifts[first_unknown] =
+
+# samples to verify
+# [1801979802, 1315634022, 635723058, 1369133069, 1125898167, 1059961393, 2089018456, 628175011, 1656478042, 1131176229]
+# (Pdb++) predicted_nums
+# [1801979802, 1315634021, 635723058, 1369133069, 1125898166, 1059961393, 2089018455, 628175010, 1656478042, 1131176228]
+
+
+def verify(outputs, index, s, s_3, s_31):
+  index = -(index + 1)
+  x_31 = (outputs[index - 31] << 1) + s_31
+  x_3 = (outputs[index - 3] << 1) + s_3
+  x = (outputs[index] << 1) + s
+  return (x_31 + x_3) % CONST_2_32 == x % CONST_2_32
+
+POSSIBLE_TRIPLETS = ((0, 1, 1), (1, 0, 1), (0, 0, 0), (1, 1, 0))
+
+def glibc_predictor(outputs):
+  cases = [([None for _ in range(34)], 0)]
+  results = None
+  max_index = len(outputs) - 33 + 1
+  while cases:
+    case = cases.pop()
+    shifts, index = case
+
+    if index == max_index:
+      result = shifts[:max_index][::-1]
+      last_outputs = outputs[-max_index:]
+      random_seed = [(out << 1) + shift for out, shift in zip(last_outputs, result)]
+      results = random_seed
+      continue
+
+    for s, s_3, s_31 in POSSIBLE_TRIPLETS:
+      new_shifts = shifts.copy()
+
+      if not verify(outputs, index, s, s_3, s_31):
+        continue
+
+      no_lack = (index + 31) - len(new_shifts) + 1
+      if no_lack > 0:
+        new_shifts.extend((None for _ in range(no_lack)))
+
+      if new_shifts[index] is not None and new_shifts[index] != s:
+        continue
+      new_shifts[index] = s
+      if new_shifts[index + 3] is not None and new_shifts[index + 3] != s_3:
+        continue
+      new_shifts[index + 3] = s_3
+      if new_shifts[index + 31] is not None and new_shifts[index + 31] != s_31:
+        continue
+      new_shifts[index + 31] = s_31
+
+      cases.append((new_shifts, index + 1))
+
+  results = results[-31:]
+
+  while True:
+    results.append((results[-3] + results[-31]) % CONST_2_32)
+    results.pop(0)
+    yield results[-1] >> 1
+
 
 def zip_many(iterable, size):
   return zip(*(tuple(iterable)[idx:] for idx in range(size)))
 
 
 def _get_glibc_output(size, seed=1):
-  proc = subprocess.Popen(['./glibc_random', str(size)],
+  proc = subprocess.Popen(['./glibc_random', str(size), str(seed)],
                           stdout=subprocess.PIPE)
   out, _ = proc.communicate()
   return [int(num) for num in out.decode('utf-8').split('\n') if num]
@@ -69,14 +159,20 @@ def cracking_custom_lcg():
 
 
 def cracking_glibc_random():
-  samples = _get_glibc_output(1000)
-  for sample_set in zip_many(samples, 16):
-    modulus, multiplier, increment = solve_with_all_unknown(sample_set)
-    # assert increment == LCG.increment
-    # assert multiplier == LCG.multiplier
-    # assert modulus == LCG.modulus
-    print(sample_set)
-
+  for seed in (1, 1232, 123, 5):
+    glibc_randoms = _get_glibc_output(52500, seed=seed)
+    # PRED_SIZE = 100
+    for pred_size in range(88, 123):
+      for_predictor = glibc_randoms[:pred_size]
+      to_compare = glibc_randoms[pred_size:]
+      for original, predicted in zip(to_compare, glibc_predictor(for_predictor)):
+        try:
+          assert original == predicted
+        except AssertionError:
+          print(f'Failed (seed: {seed}) for {pred_size}')
+          break
+      else:
+        print(f'Success (seed: {seed}) for {pred_size}')
 
 def main():
   cracking_glibc_random()
--- problem-set-1/glibc_random.c
+++ problem-set-1/glibc_random.c
@@ -1,17 +1,38 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <stdint.h>
-
 
 int main(int argc, char *argv[]) {
-  if (argc < 2 || argc > 3) {
+  if (argc < 2) {
     return 0;
   }
+
+  int amount = atoi(argv[1]);
+  int upper_limit = 344 + amount;
+  int r[upper_limit];
+  int i;
+
   if (argc == 3) {
-    srand(atoi(argv[2]));
+    r[0] = atoi(argv[2]);
+  } else {
+    // Seed
+    r[0] = 1;
   }
-  for (int i = 0; i < atoi(argv[1]); i++) {
-    printf("%d\n", rand());
+
+  for (i=1; i<31; i++) {
+    r[i] = (16807LL * r[i-1]) % 2147483647;
+    if (r[i] < 0) {
+      r[i] += 2147483647;
+    }
+  }
+  for (i=31; i<34; i++) {
+    r[i] = r[i-31];
+  }
+  for (i=34; i<344; i++) {
+    r[i] = r[i-31] + r[i-3];
+  }
+  for (i=344; i < upper_limit; i++) {
+    r[i] = r[i-31] + r[i-3];
+    printf("%d\n", ((unsigned int)r[i]) >> 1);
   }
   return 0;
 }
