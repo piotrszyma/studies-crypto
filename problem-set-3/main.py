ff19078b7fe4ab80b114b629c5a69ddb0ff455e1
Piotr Szyma <thompson2908@gmail.com>
2019-04-19 20:09:05 +0200

    Extracted methods removed from main

--- problem-set-3/main.py
+++ problem-set-3/main.py
@@ -11,10 +11,7 @@ import os
 from typing import Text
 
 import crypto_libs
-
-STORE_PASS = 'testpass'
-STORE_PATH = 'teststore.p12'
-SECRET_LEN = 16  # in bytes
+import utils
 
 MODES_CHOICES = ('aes-128-cbc', )
 OPERATION_ENC = 'enc'
@@ -22,31 +19,6 @@ OPERATION_DEC = 'dec'
 OPERATION_CHOICES = (OPERATION_DEC, OPERATION_ENC)
 OUTPUT_STDOUT = object()
 PASSWORD_DEFAULT = 'defaultpassword'
-
-
-def duplets(hex_input: str):
-  if len(hex_input) % 2 != 0:
-    hex_input = '0' + hex_input
-  iterable = iter(hex_input)
-  while True:
-    yield next(iterable) + next(iterable)
-
-
-def hex_from_bytes(bytes_input: bytes) -> str:
-  return hex(int.from_bytes(bytes_input, byteorder='big'))[2:]
-
-
-def byte_from_hex(hex_input: str) -> bytes:
-  return int(hex_input, base=16).to_bytes(length=1, byteorder='big')
-
-
-def bytes_from_hex(hex_input: str) -> bytes:
-  return functools.reduce(lambda prev, curr: prev + byte_from_hex(curr),
-                          duplets(hex_input), b'')
-
-
-def normalize_secret(secret: bytes):
-  return bytes(max(0, SECRET_LEN - len(secret))) + secret[:SECRET_LEN]
 
 
 def parse_arguments():
@@ -79,9 +51,9 @@ def get_data(parsed_args) -> bytes:
 
 def get_secret(parsed_args) -> bytes:
   if parsed_args.secret:
-    return normalize_secret(bytes_from_hex(parsed_args.secret))
+    return utils.normalize_secret(utils.bytes_from_hex(parsed_args.secret))
   print("Type secret (in hex format) to use and press ENTER.")
-  return normalize_secret(bytes_from_hex(input()))
+  return utils.normalize_secret(utils.bytes_from_hex(input()))
 
 
 def get_password(parsed_args) -> bytes:
@@ -113,9 +85,9 @@ def get_output(parsed_args):
 def get_config(parsed_args):
   config = configparser.ConfigParser()
   config.read(parsed_args.config_path)
-  secret = bytes_from_hex(config['credentials']['secret'])
+  secret = utils.bytes_from_hex(config['credentials']['secret'])
   password = config['credentials']['password']
-  return normalize_secret(secret), password.encode('utf-8')
+  return utils.normalize_secret(secret), password.encode('utf-8')
 
 
 def get_secret_and_password(parsed_args):
