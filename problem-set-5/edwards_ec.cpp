a6cb7ef2e2a7b13d6772953ab27d9e7ee4e6e466
Piotr Szyma <thompson2908@gmail.com>
2019-05-27 22:50:34 +0200

    edwards curves - gmp WIP

--- problem-set-5/edwards_ec.cpp
+++ problem-set-5/edwards_ec.cpp
@@ -4,10 +4,23 @@
 #include <assert.h>
 #include <utility>
 
-#define kPameterD -11
+#include <gmp.h>
+
+#define ED_CURV_D -300
 
 // Edward curve def
 // x_2 + y_2 = 1 + d * x_2 * y_2.
+
+int modInverse(int a, int m) 
+{ 
+  a = a % m; 
+  for (int x=1; x<m; x++) {
+    if ((a*x) % m == 1) {
+      return x; 
+    }
+  }
+  return -1;
+} 
 
 int power(int base, const unsigned int exponent, const int modulus) {
   while (base < 0) 
@@ -46,7 +59,7 @@ class F {
     }
 
     F getD() {
-      return F(kPameterD, modulus);
+      return F(ED_CURV_D, modulus);
     }
 
     F operator + (F anotherObject) {
@@ -115,24 +128,82 @@ auto modulusFactory(int modulus) {
 };
 
 
-int main(void) {
+auto gen() {
   auto F_1009 = modulusFactory(1009);
-  assert(F_1009(-1) == F_1009(1008));
-  assert(F_1009(6) != F_1009(5));
-  assert(F_1009(101) + F_1009(1000) == F_1009(92));
-  assert(F_1009(101) - F_1009(1000) == F_1009(110));
-  assert(F_1009(101) * F_1009(1000) == F_1009(100));
-  assert(F_1009(101) / F_1009(1000) == F_1009(213));
+  F_PAIR knownPoint (F_1009(1), F_1009(0)); // P
+  auto privateKey = 581; // a
+  F_PAIR publicKey = scalarMultiply(privateKey, knownPoint); // R
+  return std::make_tuple(knownPoint, publicKey, privateKey); // P, R, a
+}
+
+auto enc(F message, F_PAIR publicKey, F_PAIR knownPoint) {
+  auto randomKey = 223;
+  auto decPoint = scalarMultiply(randomKey, knownPoint); // Q = k * P
+  auto encPoint = scalarMultiply(randomKey, publicKey); // k * R
+  auto cypher = message * encPoint.first;
+  return std::make_tuple(cypher, decPoint); // (x * m, Q)
+}
+
+auto dec(F cypher, int privateKey, F_PAIR decPoint) {
+  F_PAIR privDecPoint = scalarMultiply(privateKey, decPoint);
+  F privDecPointX = privDecPoint.first;
+  std::cout << privDecPointX.getValue() << std::endl;
+  auto modInv = modInverse(privDecPointX.getValue(), 1009);
+  std::cout << modInv << std::endl;
+};
+
+
+int main(void) {
+  // Generate random modulus;
+  mpz_t randomNumber;
+  unsigned long int i, seed;
+  gmp_randstate_t r_state;
+  seed = 123456;
+  gmp_randinit_default(r_state);
+  gmp_randseed_ui(r_state, seed);
+  mpz_init(randomNumber);
+
+  for(i = 0; i < 10; ++i) {
+      mpz_urandomb(randomNumber,r_state,256);
+  }
+
+  gmp_randclear(r_state);
+  mpz_clear(randomNumber);
+
+
+  // auto F_1009 = modulusFactory(1009);
+  // auto genResult = gen();
+
+  // F_PAIR knownPoint = std::get<0>(genResult);
+  // F_PAIR publicKey = std::get<1>(genResult);
+  // int privateKey = std::get<2>(genResult);
+
+  // F message = F_1009(123);
+  // auto encResult = enc(message, publicKey, knownPoint);
+
+  // F cypher = std::get<0>(encResult);
+  // F_PAIR decPoint = std::get<1>(encResult);
+
+  // dec(cypher, privateKey, decPoint);
+
+
+  // auto F_1009 = modulusFactory(1009);
+  // assert(F_1009(-1) == F_1009(1008));
+  // assert(F_1009(6) != F_1009(5));
+  // assert(F_1009(101) + F_1009(1000) == F_1009(92));
+  // assert(F_1009(101) - F_1009(1000) == F_1009(110));
+  // assert(F_1009(101) * F_1009(1000) == F_1009(100));
+  // assert(F_1009(101) / F_1009(1000) == F_1009(213));
   
-  F_PAIR P1 (F_1009(7), F_1009(415));
-  F_PAIR P2 (F_1009(23), F_1009(487));
-  F_PAIR result = edwardsAdd(P1, P2);
+  // F_PAIR P1 (F_1009(7), F_1009(415));
+  // F_PAIR P2 (F_1009(23), F_1009(487));
+  // F_PAIR result = edwardsAdd(P1, P2);
 
-  assert(result.first.getValue() == 944);
-  assert(result.second.getValue() == 175);
+  // assert(result.first.getValue() == 944);
+  // assert(result.second.getValue() == 175);
 
-  F_PAIR multResult = scalarMultiply(5, result);
-  assert(multResult.first.getValue() == 900);
-  assert(multResult.second.getValue() == 799);
+  // F_PAIR multResult = scalarMultiply(5, result);
+  // assert(multResult.first.getValue() == 900);
+  // assert(multResult.second.getValue() == 799);
   return 0;
 };
